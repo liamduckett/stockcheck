551a33e50ad06f3dd915919a3e74af224c9609b8
Liam Duckett <liamduckett2@gmail.com>
2023-12-10 20:12:09 +0000

    introduce new variable for raw user input, to reduce mutability

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,17 @@ fn main() {
     println!("You expect {expected_pizzas} pizzas.");
     print!("How many pizzas do you currently have: ");
 
-    let mut actual_pizzas = String::new();
+    let mut raw_actual_pizzas = String::new();
 
     std::io::stdout()
         .flush()
         .unwrap();
 
     std::io::stdin()
-        .read_line(&mut actual_pizzas)
+        .read_line(&mut raw_actual_pizzas)
         .expect("Failed to read input (actual_pizzas)");
 
-    let actual_pizzas = actual_pizzas.trim();
+    let actual_pizzas = raw_actual_pizzas.trim();
 
     println!("You have {actual_pizzas} pizzas.");
 }
