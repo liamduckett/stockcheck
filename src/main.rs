84ccb088c9c5a6a0d59350ca15c8ab2f66c298a7
Liam Duckett <liamduckett2@gmail.com>
2023-12-10 19:56:02 +0000

    make the user input their pizzas, on same line as asked

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+use std::io::Write;
+
 fn main() {
     let expected_pizzas = 2;
 
     println!("You expect {expected_pizzas} pizzas.");
-    println!("How many pizzas do you currently have:");
+    print!("How many pizzas do you currently have: ");
 
     let mut actual_pizzas = String::new();
+
+    std::io::stdout()
+        .flush()
+        .unwrap();
 
     std::io::stdin()
         .read_line(&mut actual_pizzas)
