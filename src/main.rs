b9ffd2ca18f21b6a6b6373104ef09cc61b4e8c70
Liam Duckett <liamduckett2@gmail.com>
2023-12-10 19:54:46 +0000

    ask the user how many pizzas they have

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,13 @@ fn main() {
     let expected_pizzas = 2;
 
     println!("You expect {expected_pizzas} pizzas.");
+    println!("How many pizzas do you currently have:");
+
+    let mut actual_pizzas = String::new();
+
+    std::io::stdin()
+        .read_line(&mut actual_pizzas)
+        .expect("Failed to read input (actual_pizzas)");
+
+    println!("You have {} pizzas.", actual_pizzas.trim());
 }
